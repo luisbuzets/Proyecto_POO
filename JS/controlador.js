b6706c583c2d9db79dff3075341414b4c2bbde01
LUIS BUZETA <72575550+luisbuzets@users.noreply.github.com>
2021-10-29 01:12:11 -0600

    push master

--- JS/controlador.js
+++ JS/controlador.js
@@ -1,4 +1,5 @@
-
+/* este parametro ses para probar los click no es necesario usarlo */
+/*
 function seleccionMenu(id){
     console.log('mostrar', id);
 document.getElementById('menu-comida').style.display = 'none';
@@ -7,3 +8,25 @@ document.getElementById('menu-medicamentos').style.display = 'none';
 document.getElementById('menu-tecnologia').style.display = 'none';
 document.getElementById(id).style.display ='block';
 }
+*/
+/* este parametro es cunado el momento de dar click en allgun menu se pueda 
+desaparecer todo el menu para diseñar a otra pagina*/
+function mostrarDetalleMenu(){
+    
+    document.getElementById('menu-supermercado').style.display = 'none';
+    document.getElementById('menu-medicamentos').style.display = 'none';
+    document.getElementById('menu-tecnologia').style.display = 'none'; 
+    document.getElementById('menu-comida').style.display ='none';
+    document.getElementById('volver').style.display = 'flex';
+
+}
+/*
+esta funncion es para probar un boton para regresar 
+function volver(){
+    document.getElementById('menu-supermercado').style.display = 'flex';
+    document.getElementById('menu-medicamentos').style.display = 'flex';
+    document.getElementById('menu-tecnologia').style.display = 'flex'; 
+    document.getElementById('menu-comida').style.display ='flex';
+
+}
+*/
