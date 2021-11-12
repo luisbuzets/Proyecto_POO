2ace8cdd8ea7e26f18444c02da2f6a60f617dff1
LUIS BUZETA <72575550+luisbuzets@users.noreply.github.com>
2021-11-11 21:58:58 -0600

    selccionar el menu + estilos de card

--- JS/controlador.js
+++ JS/controlador.js
@@ -1,32 +1,40 @@
-/* este parametro ses para probar los click no es necesario usarlo */
-/*
-function seleccionMenu(id){
-    console.log('mostrar', id);
-document.getElementById('menu-comida').style.display = 'none';
-document.getElementById('menu-supermercado').style.display = 'none';
-document.getElementById('menu-medicamentos').style.display = 'none';
-document.getElementById('menu-tecnologia').style.display = 'none';
-document.getElementById(id).style.display ='block';
-}
-*/
-/* este parametro es cunado el momento de dar click en allgun menu se pueda 
-desaparecer todo el menu para diseñar a otra pagina*/
-function mostrarDetalleMenu(){
-    
+
+function mostrarComida(){
     document.getElementById('menu-supermercado').style.display = 'none';
     document.getElementById('menu-medicamentos').style.display = 'none';
     document.getElementById('menu-tecnologia').style.display = 'none'; 
     document.getElementById('menu-comida').style.display ='none';
-    document.getElementById('volver').style.display = 'flex';
+    document.getElementById('alerta').style.display ='none';
+    document.getElementById('detalle-comida').style.display = 'flex';
+    document.getElementById('carrito').style.display = 'flex';
+    
+}
+function mostrarSupermercado(){
+    document.getElementById('menu-supermercado').style.display = 'none';
+    document.getElementById('menu-medicamentos').style.display = 'none';
+    document.getElementById('menu-tecnologia').style.display = 'none'; 
+    document.getElementById('menu-comida').style.display ='none';
+    document.getElementById('alerta').style.display ='none';
+    document.getElementById('detalle-supermercado').style.display = 'flex';
+    document.getElementById('carrito').style.display = 'flex';
 
 }
-/*
-esta funncion es para probar un boton para regresar 
-function volver(){
-    document.getElementById('menu-supermercado').style.display = 'flex';
-    document.getElementById('menu-medicamentos').style.display = 'flex';
-    document.getElementById('menu-tecnologia').style.display = 'flex'; 
-    document.getElementById('menu-comida').style.display ='flex';
+function mostrarMedicamentos(){
+    document.getElementById('menu-supermercado').style.display = 'none';
+    document.getElementById('menu-medicamentos').style.display = 'none';
+    document.getElementById('menu-tecnologia').style.display = 'none'; 
+    document.getElementById('menu-comida').style.display ='none';
+    document.getElementById('alerta').style.display ='none';
+    document.getElementById('detalle-medicamentos').style.display = 'flex';
+    document.getElementById('carrito').style.display = 'flex';
 
 }
-*/
+function mostrarTecnologia(){
+    document.getElementById('menu-supermercado').style.display = 'none';
+    document.getElementById('menu-medicamentos').style.display = 'none';
+    document.getElementById('menu-tecnologia').style.display = 'none'; 
+    document.getElementById('menu-comida').style.display ='none';
+    document.getElementById('alerta').style.display ='none';
+    document.getElementById('detalle-tecnologia').style.display ='flex'
+    document.getElementById('carrito').style.display = 'flex';
+}
